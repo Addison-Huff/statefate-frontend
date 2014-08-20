3c05edd7f4bd7a190ebf200665eba7257a4781ba
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2014-08-19 21:07:31 -0400

    added bower, added basic coffee files

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,9 +58,6 @@ module.exports = function(grunt) {
 		grunt.file.copy('bower_components/backbone/backbone.js', buildDir + '/js/lib/backbone.js');
 		grunt.file.copy('bower_components/underscore/underscore.js', buildDir + '/js/lib/underscore.js');
 		grunt.file.copy('bower_components/jquery/dist/jquery.js', buildDir + '/js/lib/jquery.js');
-		grunt.file.copy('bower_components/d3/d3.js', buildDir + '/js/lib/d3.js');
-		grunt.file.copy('bower_components/nvd3/nv.d3.js', buildDir + '/js/lib/nv.d3.js');
-		grunt.file.copy('bower_components/nvd3/nv.d3.css', buildDir + '/css/lib/nv.d3.css');
 	});
 
 	grunt.task.registerTask('haml:compile', 'Compile haml templates', function() {
@@ -96,75 +93,6 @@ module.exports = function(grunt) {
 			grunt.file.write(grunt.config.get('buildDir') + '/' + targetPath, js);
 		});
 	});
-
-	grunt.task.registerTask('config:schema', 'Set schemas', function() {
-		var done = this.async();
-		cprocess.exec('search-cmd set-schema expense config/riak-schema.erl', function(err, stdout) {
-			if(err) {
-				console.error(err);
-			}
-			console.log(stdout);
-			done();
-		});
-	});
-
-	grunt.task.registerTask('drop-bucket', 'Delete a bucket', function() {
-		var deleteObj = function(key) {
-			var emitter = new events.EventEmitter();
-			var options = {
-				hostname: 'expenses',
-				path: '/db/buckets/expense/keys/' + key,
-				method: 'DELETE'
-			};
-			http.request(options, function(res) {
-				emitter.emit('deleted', key);
-			}).end();
-			return emitter;
-		};
-		var getKeys = function() {
-			var emitter = new events.EventEmitter();
-			var options = {
-				hostname: 'expenses',
-				path: '/db/buckets/expense/keys?keys=true',
-				method: 'GET'
-			};
-			http.request(options, function(res) {
-				var body = '';
-				res.on('data', function(chunk) {
-					body += chunk;
-				});
-				res.on('end', function() {
-					var keys = JSON.parse(body)['keys'];
-					keys.forEach(function(key) {
-						emitter.emit('key', key);
-					});
-					emitter.emit('end');
-				});
-			}).end();
-			return emitter;
-		};
-
-		var e = getKeys();
-		var done = this.async();
-		var queue = [];
-		e.on('key', function(key) {
-			queue.push(key);
-			var e = deleteObj(key);
-			e.on('deleted', function(key) {
-				queue.splice(queue.indexOf(key), 1);
-				console.log('deleted', key);
-				if(queue.length == 0) {
-					done();
-				}
-			});
-		});
-		e.on('end', function() {
-			if(queue.length == 0) {
-				done();
-			}
-		});
-	});
-
 
 	grunt.task.registerTask('default', ['clean', 'copy:html', 'haml:compile', 'coffee:compile', 'copy:dependencies', 'sass', 'watch']);
 };
